fbc2ee90a9f48728fae919994339b41da989f10b
WeiHungLoh <weihung789@hotmail.com>
2025-01-17 22:33:58 +0800

    Add mark tasks as done feature

--- src/main/java/Duet.java
+++ src/main/java/Duet.java
@@ -7,21 +7,45 @@ public class Duet {
         System.out.println("What can I do for you?");
         
         Scanner scan = new Scanner(System.in);
-        ArrayList<String> messages = new ArrayList<>();
+        ArrayList<Task> messages = new ArrayList<>();
 
         while (true) {
             String message = scan.nextLine(); 
-            messages.add(message); // store message into ArrayList
+            String[] command = message.split(" ");      
 
             if (message.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             } else if (message.equals("list")) { 
+                System.out.println("Here are the tasks in your list:");
+
                 for (int i = 0; i < messages.size(); i++) {
-                    String index = String.valueOf(i + 1);
-                    System.out.println(index + ". " + messages.get(i));
+                    String index = String.valueOf(i + 1); // increment index since it starts from 1
+                    Task taskName = messages.get(i);
+                    System.out.println(index + "." + "[" + taskName.getStatusIcon() + "] " + taskName.getDescription());
                 }
+            } else if (command[0].equals("mark")) {
+                int idx = Integer.parseInt(command[1]) - 1; // decrement index since ArrayList is zero-indexed
+
+                if (Integer.parseInt(command[1]) > messages.size()) {
+                    throw new IndexOutOfBoundsException();
+                }
+
+                messages.get(idx).markAsDone();;
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("  [" + messages.get(idx).getStatusIcon() + "] " + messages.get(idx).getDescription());
+            } else if (command[0].equals("unmark")) {
+                int idx = Integer.parseInt(command[1]) - 1; // decrement index since ArrayList is zero-indexed
+
+                if (Integer.parseInt(command[1]) > messages.size()) {
+                    throw new IndexOutOfBoundsException();
+                }
+
+                messages.get(idx).unmarkAsDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("  [" + messages.get(idx).getStatusIcon() + "] " + messages.get(idx).getDescription());
             } else {
+                messages.add(new Task(message));
                 System.out.println("added: " + message);
             }
         }
