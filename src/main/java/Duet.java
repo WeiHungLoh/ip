cb8b20f7badacb59aeb150bc79902b2887bcc855
WeiHungLoh <weihung789@hotmail.com>
2025-01-17 20:45:14 +0800

    Add echos commands

--- src/main/java/Duet.java
+++ src/main/java/Duet.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Duet {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Duet");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        
+        Scanner scan = new Scanner(System.in);
+
+        while (true) {
+            String message = scan.nextLine(); 
+
+            if (message.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println(message);
+            }
+        }
+
+        scan.close();
     }
 }
