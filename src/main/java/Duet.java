82ccd79f2d6a28dd9d63bd78be37e612aebac84d
WeiHungLoh <weihung789@hotmail.com>
2025-01-17 21:19:13 +0800

    Add ability to store text by user

--- src/main/java/Duet.java
+++ src/main/java/Duet.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duet {
@@ -6,15 +7,22 @@ public class Duet {
         System.out.println("What can I do for you?");
         
         Scanner scan = new Scanner(System.in);
+        ArrayList<String> messages = new ArrayList<>();
 
         while (true) {
             String message = scan.nextLine(); 
+            messages.add(message); // store message into ArrayList
 
             if (message.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (message.equals("list")) { 
+                for (int i = 0; i < messages.size(); i++) {
+                    String index = String.valueOf(i + 1);
+                    System.out.println(index + ". " + messages.get(i));
+                }
             } else {
-                System.out.println(message);
+                System.out.println("added: " + message);
             }
         }
 
