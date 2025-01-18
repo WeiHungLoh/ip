11fc15341005398766955b199017d1c8c6b108d0
WeiHungLoh <weihung789@hotmail.com>
2025-01-18 15:36:49 +0800

    Add "from" and "to" details

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -20,6 +20,6 @@ public class Event extends Task {
      */
     @Override
     public String toString() {
-        return "[E]" + super.toString();
+        return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
     }
 }
