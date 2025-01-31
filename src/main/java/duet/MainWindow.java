d2850c7008e58014cf55c48e03924eb4a92c1d42
WeiHungLoh <weihung789@hotmail.com>
2025-01-31 19:31:48 +0800

    Add GUI to Duet chatbot

---
+++ src/main/java/duet/MainWindow.java
@@ -0,0 +1,67 @@
+package duet;
+
+import duet.exception.EmptyInputException;
+import duet.exception.InvalidInputException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+/**
+ * Controller for the main GUI.
+ */
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Duet duet;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Duke instance */
+    public void setDuet(Duet d) {
+        duet = d;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     *
+     * @throws InvalidInputException If deadline or event has no by or from date.
+     * @throws EmptyInputException If user input is empty.
+     */
+    @FXML
+    private void handleUserInput() throws EmptyInputException, InvalidInputException {
+        String input = userInput.getText();
+        String response = duet.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, dukeImage)
+        );
+        userInput.clear();
+
+        if (input.equals("bye")) {
+            closeWindow();
+        }
+    }
+
+    private void closeWindow() {
+        Stage stage = (Stage) userInput.getScene().getWindow();
+        stage.close();
+    }
+}
